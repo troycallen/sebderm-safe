62132ef18afa0e34dbaf841954b0566726bd40f1
troycallen <165961938+troycallen@users.noreply.github.com>
2024-07-29 07:25:29 -0400

    Separate files into public and src

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const ingredientsRouter = require('./routes/api');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
 app.use('/api', ingredientsRouter);
